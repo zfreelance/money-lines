d4d541e700e1da4e2508785b3e06bdda15732b70
zNadir <115375151+znadir@users.noreply.github.com>
2024-12-12 20:50:13 -0500

    Save Data in Excel File

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-import time
 from selenium.webdriver.remote.webelement import WebElement
 from colorama import Fore, Style
+import time
+import os
+import csv
+from typing import List, Dict
 
 
 IS_HEADLESS = True # Hide automated Browser
 SAFE_TITLES = ["Hard Rock"] # list of titles page scraped should give
 UPDATE_INTERVAL = 10 # how often sportspage excel should be update (in Seconds)
+EXCEL_FILENAME = "../sportspage_nba.csv"
 
 def parseEvent(event: WebElement):
+    """
+    Parse event element
+    """
     team_names_el = event.find_elements(By.CSS_SELECTOR, ".participants .show-for-medsmall")
 
     team_names = [team_name_el.text for team_name_el in team_names_el]
@@ -44,18 +51,51 @@ def parseEvent(event: WebElement):
 
     return teams
 
-def update_excel(driver):
+def save_excel_file(events_data: List[List[Dict]]):
+    """
+    Save Data in Excel File
+    """
+    file_path = os.path.join(os.path.dirname(__file__), EXCEL_FILENAME)
+
+    first_rows = [["Team Name","Spread","Total Points","Win"], []]
+
+    excel_rows = []
+
+    for event_data in events_data:
+        for i, team in enumerate(event_data):
+            excel_rows.append(list(team.values()))
+
+            # separate each dual
+            if i == 1:
+                excel_rows.append([])
+
+    with open(file_path, mode="w", newline="") as file:
+        writer = csv.writer(file, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        writer.writerows(first_rows)
+        writer.writerows(excel_rows)
+
+    print(f"{Fore.GREEN}[+] Saved Excel file with sportspage{Style.RESET_ALL}")
+
+def update_excel_events(driver):
+    """
+    Select events and update excel
+    """
     events_list = driver.find_elements(By.CSS_SELECTOR, 'div.events-group .column .hr-market-view')
 
     print(f"{Fore.GREEN}[+] Events found: {len(events_list)}{Style.RESET_ALL}")
 
+    events_data = []
+
     for event in events_list:
         try:
-            data = parseEvent(event)
+            event_data = parseEvent(event)
         except Exception as e:
             print(f"{Fore.RED}[!] Failed to parse event. Ignoring. {e}{Style.RESET_ALL}")
 
-        print(data)
+        events_data.append(event_data)
+
+    save_excel_file(events_data)
 
 def main():
     print(f"{Fore.GREEN}[+] Launching Browser...{Style.RESET_ALL}")
@@ -83,7 +123,7 @@ def main():
     time.sleep(1) # ensure all elements are still there
 
     while True:
-        update_excel(driver)
+        update_excel_events(driver)
 
         print(f"{Fore.YELLOW}[-] Waiting {UPDATE_INTERVAL} seconds before updating excel again {Style.RESET_ALL}")
 
